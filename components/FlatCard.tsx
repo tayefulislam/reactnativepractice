af85bcf8175a44e2717eb9c67db04528ee3a14c2
tayefulislam <kazitayefulislam12@gmail.com>
2024-09-13 16:02:31 +0600

    initialized the project

---
+++ components/FlatCard.tsx
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native';
+import React, { Component } from 'react';
+
+export default class FlatCard extends Component {
+  render() {
+      return (
+          <View>
+              <Text>FlatCard</Text>
+          </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({});
