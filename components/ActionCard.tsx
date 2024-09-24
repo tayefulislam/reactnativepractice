f7365e97fd75abfaff806e1ba972df82a3b5dd15
tayefulislam <kazitayefulislam12@gmail.com>
2024-09-24 19:25:01 +0600

    size increase

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -1,19 +1,37 @@
-import { Text, StyleSheet, View,Pressable, Button, TouchableOpacity, Linking } from 'react-native'
+import { Text, StyleSheet, View, Linking, Image, TouchableOpacity } from 'react-native'
 import React, { Component } from 'react'
 
 export default class ActionCard extends Component {
     render() {
-      
         function openWebsite(websiteLink: string) {
             Linking.openURL(websiteLink);
-        }
+      }
+        
     return (
       <View>
-            <Text style={styles.sectionTitle}>ActionCard 2</Text>
-            <View>
-                <Pressable onPress={()=>openWebsite('https://blooddonationen1.web.app/')}>
-                    <Text>Learn More</Text>
-               </Pressable>
+            <Text style={styles.headingText} >Blog Card</Text>
+            <View style={[styles.card, styles.elevatedCard]}>
+                <View style={styles.headingContainer}>
+                    <Text style={styles.headerText}>Learn More About Flood In Bangladesh</Text>
+                </View>
+
+                <Image style={styles.cardImage}
+                    source={{
+                    uri:'https://phero-web.nyc3.cdn.digitaloceanspaces.com/website-prod-images/public/files/1640512633829.jpg'
+                    }} />
+                
+                <View style={styles.bodyContainer}>
+                    <Text numberOfLines={2} >Sometimes, you might be getting encoded image data from a REST API call. You can use the 'data:' URI scheme to use these images. Same as for network resources, you will need to manually specify the dimensions of your image.</Text>
+                    
+                </View>
+                <View style={styles.footerContainer}>
+                    <TouchableOpacity
+                    onPress={()=>openWebsite('https://reactnative.dev/docs/touchableopacity')}>
+                       <Text> Profile Link</Text>
+                    </TouchableOpacity>
+                </View>
+
+
             </View>
       </View>
     )
@@ -21,11 +39,29 @@ export default class ActionCard extends Component {
 }
 
 const styles = StyleSheet.create({
-    sectionTitle: {
-        fontWeight: 'bold',
-        fontSize: 30,
-        marginTop: 12,
-        paddingHorizontal: 10,
-}
-
+    headingText: {
+        fontSize: 25,
+        fontWeight: 'semibold',
+        paddingHorizontal:10,
+    },
+    card: {
+        height: 480,
+        width: 340,
+        backgroundColor: '#87A2FF',
+        borderRadius: 10,
+        marginHorizontal: 30,
+        marginVertical:8,
+       
+    
+    },
+    elevatedCard: {},
+    headingContainer: {},
+    headerText: {},
+    cardImage: {
+        height:500,
+    },
+    bodyContainer: {},
+    footerContainer:{},
+    
+    
 })
